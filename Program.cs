4737840f4178bf75bbef9f5bde59257ae6a9c0e6
Simon Isupov <is_se@mail.ru>
2024-03-21 21:55:18 +0300

    3

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 3: Напишите программу, которая перевернёт одномерный массив 
+// (первый элемент станет последним, второй – предпоследним и т.д.)
+
+
+int[] CreateArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+    System.Console.WriteLine();
+}
+
+int [] TurnArray (int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int x;
+        x=arr[i];
+        arr[i]=arr[arr.Length-1-i];
+        arr[arr.Length-1-i]=x;
+    }
+    return arr;
+}
+
+System.Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input minimal value: ");
+int min = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Input maximal value: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = CreateArray(size, min, max);
+PrintArray(arr);
+System.Console.WriteLine(TurnArray(arr));
